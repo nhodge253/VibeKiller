a6f9aed64933d467a69035d155436e2c4b9f6385
shao sosa <shao sosa>
2017-05-04 00:25:10 -0700

    Bootstrapped project and got Angular rolling

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vibeKillerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vibeKillerApp
+ */
+angular.module('vibeKillerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vibeKillerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vibeKillerApp
+ */
+angular.module('vibeKillerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
